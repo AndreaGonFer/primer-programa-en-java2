58874ad218b8f30deda7503696b30e3969b69b0c
Andrea <andreagf91@gmail.com>
2021-10-18 12:35:33 +0200

    Grupo favorito

--- Prueba.java
+++ Prueba.java
@@ -2,9 +2,9 @@ public class Prueba {
 
 	public static void main(String args[]) {
 
-		System.out.println("Andrea Gonzalez Fernandez");
 		System.out.println("Villabalter 1991");
 		System.out.println("Trobajo del Camino Windows10");
+		System.out.println("Estopa");
 		
 	}
 }
